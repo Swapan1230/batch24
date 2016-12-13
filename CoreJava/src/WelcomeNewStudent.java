8427d7b04c0945bea9bb3754a924ed6b16dd5100
jayramrout <jayram.rout@gmail.com>
2016-12-12 22:07:52 -0500

    Update

---
+++ CoreJava/src/WelcomeNewStudent.java
@@ -0,0 +1,22 @@
+/**
+ * This is for Welcomeing Student....
+ * @author jrout
+ *
+ */
+public class WelcomeNewStudent {
+	
+	/**
+	 * This is my main method...
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//I am going to do something
+		int age = 10;
+		int double_ = 234;
+		String name = "Ashutosh";
+		
+		
+		String value = "H2kInfosys"; //This is value for H2kInfosys
+		System.out.println(value.substring(3, 7));
+	}
+}
---
+++ CoreJava/src/jrout/datatype/DataTypeExample.java
@@ -0,0 +1,10 @@
+package jrout.datatype;
+
+public class DataTypeExample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
